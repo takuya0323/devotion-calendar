1cac0a052b7338fa0e5538d42fb2694802780a35
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-09-06 16:40:19 +0900

    バリデーションの修正

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -5,8 +5,8 @@ class Impression < ApplicationRecord
   validates :part,numericality: { other_than: 0 , message: "can't be blank"}
    
   with_options presence: true do
-    validates :chapter,format: { with: /\A[\d~]+\z/, message: "は半角数字とチルダ（~）のみが許可されています。" }
-    validates :verse,format: { with: /\A[\d~]+\z/, message: "は半角数字とチルダ（~）のみが許可されています。" }
+    validates :chapter,format: { with: /\A[\d~〜]+\z/, message: "は半角数字とチルダ（~）のみが許可されています。" }
+    validates :verse,format: { with: /\A[\d~〜]+\z/, message: "は半角数字とチルダ（~）のみが許可されています。" }
     validates :feedback
   end  
 
