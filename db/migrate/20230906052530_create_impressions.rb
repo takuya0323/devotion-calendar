03a878ca5e429e044256eec46f862af898b36d45
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-09-06 15:05:17 +0900

    マイグレーションファイルの編集と、マイグレーションの実行

--- db/migrate/20230906052530_create_impressions.rb
+++ db/migrate/20230906052530_create_impressions.rb
@@ -1,7 +1,14 @@
 class CreateImpressions < ActiveRecord::Migration[6.0]
   def change
     create_table :impressions do |t|
+      t.integer :part_id, null: false
+      t.string :chapter, null: false
+      t.string :verse, null: false
+      t.text :feedback, null:false
+      t.text :prayer
       
+      t.datetime :start_time
+
       t.timestamps
     end
   end
