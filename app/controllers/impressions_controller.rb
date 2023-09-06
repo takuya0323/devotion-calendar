75c511f2c9bc4c16234370c2490062bf65b1fd41
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-09-06 15:39:35 +0900

    createアクションの設定

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -7,5 +7,16 @@ class ImpressionsController < ApplicationController
   def new
     @inpression = Impression.new
   end
+
+  def create
+    Blog.create(impression_parameter)
+    redirect_to root_path
+  end
+
+  private
+
+  def impression_parameter
+    params.require(:impression).permit(:part_id, :chapter, :verse, :feedback, :prayer, :start_time)
+  end
   
 end
