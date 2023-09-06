fd00c51f1a2d445b07b1291e3f8682f34b71ef62
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-09-06 15:21:13 +0900

    indexアクションと、newアクションの設定

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,4 +1,11 @@
 class ImpressionsController < ApplicationController
   def index
+    @impressions = Impression.all
+    @impression = Impression.new
   end
+
+  def new
+    @inpression = Impression.new
+  end
+  
 end
