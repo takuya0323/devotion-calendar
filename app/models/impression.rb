7ab147ef601bd92de0930c7a0baaf67da86af348
takuya0323 <132263806+takuya0323@users.noreply.github.com>
2023-09-07 20:13:49 +0900

    投稿をカレンダー上に表示する

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -2,7 +2,7 @@ class Impression < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :part
 
-  validates :part,numericality: { other_than: 0 , message: "can't be blank"}
+  validates :part_id,numericality: { other_than: 0 , message: "can't be blank"}
    
   with_options presence: true do
     validates :chapter,format: { with: /\A[\d~〜]+\z/, message: "は半角数字とチルダ（~）のみが許可されています。" }
